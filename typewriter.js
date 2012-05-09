804564441dc11b3dfaee87e7339e0175ac808973
Josh McCarthy <mccarthy.josh@gmail.com>
2012-05-08 22:15:31 -0400

    Don't use canvas anymore

--- typewriter.js
+++ typewriter.js
@@ -1,106 +1,56 @@
 // Typewriter.js
-// A typewriter simulator on canvas
+// A simple typewriter simulator
 // Author: Josh McCarthy
 // licensed under the MIT license
 
 (function(window){
 
-	var ctx,
-		options = {
-			start: 0,
-			left: 0,
-			top: 100,
-			space: 35,
-			shadow: true,
-			shadowX: 2,
-			shadowY: 2,
-			textColor: 'rgba(0,0,0,1.0)',
-			font: '80px Ubuntu Mono, monospace',
-			callback: function(){}
-		};
-
-	var TypeWriter = function TypeWriter(canvas, defaults)
+	var _element;
+	var _callback;
+	var TypeWriter = function TypeWriter(elementId)
 	{
-		ctx = document.getElementById(canvas).getContext('2d');
-		options = setOptions(options, defaults);
-		ctx.font = options.font;
-		this.setColor(options.textColor);
-		ctx.fillStyle = 'rgba(255,255,255,1)';
+		this.setPaper(elementId);
 	};
 
-	TypeWriter.prototype.type = function(word)
+	TypeWriter.prototype.type = function(word, callback)
 	{
         var arr = [],
         i = 0,
         length;
-
+        this.setCallback(callback);
         for (i, length = word.length; i < length; i++)
         {
             arr.push(word.charAt(i));
         }
-		typeWord(this, word, options.start, options.left);
+		typeWord(this, word, 0);
 	};
 
-	function typeWord(tw, word, start, left)
+	TypeWriter.prototype.setPaper = function(elementId)
 	{
-		if (typeof start === 'undefined')
-		{
-			start = 0;
-		}
-		if (typeof left === 'undefined')
-		{
-			left = 0;
-		}
-		if (options.shadow)
-		{
-			tw.setColor('rgba(0,0,0,1.0)');
-			ctx.fillText(word[start], left + options.shadowX, options.top + options.shadowY);
-			tw.setColor(options.textColor);
-		}
+		_element = $('#' + elementId);
+	};
 
-		ctx.fillText(word[start], left, options.top);
+	TypeWriter.prototype.setCallback = function(callback)
+	{
+		_callback = callback;
+	};
+
+	function typeWord(tw, word, start)
+	{
+		_element.html(_element.html() + word[start]);
 		start++;
-		left += options.space;
 
 		if (start < word.length)
 		{
-			setTimeout(function(){typeWord(tw, word, start, left);}, random(50, 200));
+			setTimeout(function(){typeWord(tw, word, start);}, random(50, 200));
 		}
 		else
 		{
-			options.callback();
+			if (typeof _callback !== 'undefined')
+			{
+				_callback();
+			}
 		}
-	}
-
-	TypeWriter.prototype.setColor = function(color)
-	{
-		if (typeof ctx !== 'undefined')
-		{
-			ctx.fillStyle = color;
-		}
-	};
-
-	TypeWriter.prototype.setFont = function(font)
-	{
-		if (typeof ctx !== 'undefined')
-		{
-			ctx.font = font;
-		}
-	};
-
-	TypeWriter.prototype.clear = function()
-	{
-		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-	};
-
-	function setOptions(defaults, options)
-	{
-		var prop;
-		for (prop in defaults)
-		{
-			defaults[prop] = typeof options[prop] === 'undefined' ? defaults[prop] : options[prop];
-		}
-		return defaults;
 	}
 
 	function random(min, max, round)
