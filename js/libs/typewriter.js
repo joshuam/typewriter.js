c09beed650970a353376b0003b4d5ef5316c80e1
Josh McCarthy <mccarthy.josh@gmail.com>
2012-05-08 22:13:43 -0400

    Set up site

---
+++ js/libs/typewriter.js
@@ -0,0 +1,71 @@
+// Typewriter.js
+// A typewriter simulator on canvas
+// Author: Josh McCarthy
+// licensed under the MIT license
+
+(function(window){
+
+	var _element;
+	var _callback;
+	var TypeWriter = function TypeWriter(elementId)
+	{
+		this.setPaper(elementId);
+	};
+
+	TypeWriter.prototype.type = function(word, callback)
+	{
+        var arr = [],
+        i = 0,
+        length;
+        this.setCallback(callback);
+        for (i, length = word.length; i < length; i++)
+        {
+            arr.push(word.charAt(i));
+        }
+		typeWord(this, word, 0);
+	};
+
+	TypeWriter.prototype.setPaper = function(elementId)
+	{
+		_element = $('#' + elementId);
+	};
+
+	TypeWriter.prototype.setCallback = function(callback)
+	{
+		_callback = callback;
+	};
+
+	function typeWord(tw, word, start)
+	{
+		_element.html(_element.html() + word[start]);
+		start++;
+
+		if (start < word.length)
+		{
+			setTimeout(function(){typeWord(tw, word, start);}, random(50, 200));
+		}
+		else
+		{
+			if (typeof _callback !== 'undefined')
+			{
+				_callback();
+			}
+		}
+	}
+
+	function random(min, max, round)
+	{
+		var x = Math.random() * (max-min) + min;
+		if (round)
+		{
+			x = Math.floor(x);
+		}
+		return x;
+	}
+
+	if (typeof window.TypeWriter === 'undefined')
+	{
+	window.TypeWriter = TypeWriter;
+	}
+
+}(window));
---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    tw = new TypeWriter('logo');
+
+    function typeHeader()
+    {
+        tw.type("typewriter.js", function(){
+            tw.setPaper("text");
+            tw.type("brought to you by:", function(){
+                tw.setPaper("name");
+                tw.setCallback();
+                tw.type('@mccarthyjm');
+            });
+        });
+    }
+
+    $('#paper').on('click', function(){
+        setTimeout(typeHeader, 150);
+    });
+
+    setTimeout(typeHeader, 500);
+});
